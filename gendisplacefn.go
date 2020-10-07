5a15b12c0245f3585bbcfa74d850de166c4cd2a7
attackgnome <attackgnome@users.noreply.github.com>
2020-10-07 10:53:07 -0600

    Add files via upload

--- gendisplacefn.go
+++ gendisplacefn.go
@@ -7,20 +7,20 @@ import (
 
 func main() {
 	var a float64
-	var v_o float64
-	var s_o float64
+	var vO float64
+	var sO float64
 	var t float64
 
 	fmt.Println("Enter a value for acceleration: ")
 	fmt.Scan(&a) //input from terminal
 
 	fmt.Println("Enter a value for velocity: ")
-	fmt.Scan(&v_o) //input from terminal
+	fmt.Scan(&vO) //input from terminal
 
 	fmt.Println("Enter a value for displacement: ")
-	fmt.Scan(&s_o) //input from terminal
+	fmt.Scan(&sO) //input from terminal
 
-	fn := GenDisplaceFn(a, v_o, s_o)
+	fn := GenDisplaceFn(a, vO, sO)
 
 	fmt.Println("Enter a value for time: ")
 	fmt.Scan(&t) //input from terminal
@@ -30,9 +30,9 @@ func main() {
 }
 
 // GenDisplaceFn computes displace as a function of time
-func GenDisplaceFn(a, v_o, s_o float64) func(float64) float64 {
+func GenDisplaceFn(a, vO, sO float64) func(float64) float64 {
 	fn := func(t float64) float64 {
-		return 0.5*a*math.Pow(t, 2) + v_o*t + s_o
+		return 0.5*a*math.Pow(t, 2) + vO*t + sO
 	}
 	return fn
 }
